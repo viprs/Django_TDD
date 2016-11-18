9baffc90a9589090931e32bf91a7d2bd3b97f4b5
viprsshr <viprsshr@163.com>
2016-11-18 18:01:05 +0800

    使用Django的模版返回给客户端网页
    重构HttpResponse手工生成网页的部分，用Django模版替代

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,8 +1,10 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
+
 
 # Create your tests here.
 # run command: python3 manage.py test
@@ -17,3 +19,11 @@ class HomePageTest(TestCase):
         self.assertTrue(response.content.startswith(b'<html>'))
         self.assertIn(b'<title>To-Do lists</title>', response.content)
         self.assertTrue(response.content.endswith(b'</html>'))
+
+    def test_home_page_returns_correct_html_with_render_to_string(self):
+        request = HttpRequest()
+        response = home_page(request)
+        expected_html = render_to_string('home.html')
+        # render_to_string 把文件从磁盘上加载到内存
+        self.assertEqual(response.content.decode(), expected_html)
+        # .decode() 把response.content中的字节转换成Python中的unicode字符串
