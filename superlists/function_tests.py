f6a3e443da6f8fc02d78b09d5ef691105b56ff4e
viprsshr <viprsshr@163.com>
2016-11-18 17:45:29 +0800

    Revert "编写功能测试用例"
    This reverts commit 5b8d28edbe545552403de15d9e60d96299f08045.

--- superlists/function_tests.py
+++ superlists/function_tests.py
@@ -3,7 +3,7 @@
 __author__ = 'Samren'
 import unittest
 from selenium import webdriver
-from selenium.webdriver.common.keys import keys
+
 
 class DjangoTestCase(unittest.TestCase):
 
@@ -22,31 +22,7 @@ class DjangoTestCase(unittest.TestCase):
         
         #她注意到网页的标题和头部都包含“To-Do”这个词
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_text)
-
-        #应用邀请她输入一个待办事项
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'    
-        )
-
-        #Alice输入了'Buy peacock feathers'
-        inputbox.send_keys('Buy peacock feathers')
-
-        #她按回车后，更新了
-        inputbox.send_keys(keys.ENTER)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1:Buy peacock feathers' for row in rows)
-        )
-
-
-        #这时，页面中又显示了一个文本框，可以继续输入待办事项
-        self.fail('Finish the test!')
+        #self.fail('Finish the test!')
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
