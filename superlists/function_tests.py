962aa24364d8b290c774c59baee91ba4907458be
viprsshr <viprsshr@163.com>
2016-11-18 16:35:11 +0800

    加入了用户故事，预期失败
    预期失败就是意料之中的失败。

--- superlists/function_tests.py
+++ superlists/function_tests.py
@@ -9,14 +9,20 @@ class DjangoTestCase(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-
+        self.browser.implicitly_wait(3)
+        
     def tearDown(self):
         self.browser.quit()
 
-    def test_PageTitle(self):
+    def test_can_start_a_list_and_retrieve_it_later(self):
+    
+        #Alice听说了一个很酷的在线待办事项应用
+        #于是，她打开了网站首页
         self.browser.get('http://localhost:8000')
-        self.assertIn('Django', self.browser.title)
-
+        
+        #她注意到网页的标题和头部都包含“To-Do”这个词
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
