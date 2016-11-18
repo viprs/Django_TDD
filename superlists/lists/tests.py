d56c44bd5ecb79ca462265300eb45f706e08cdea
viprsshr <viprsshr@163.com>
2016-11-18 22:48:22 +0800

    在视图函数中接收POST数据，并保存

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -29,12 +29,22 @@ class HomePageTest(TestCase):
         self.assertEqual(response.content.decode(), expected_html)
         # .decode() 把response.content中的字节转换成Python中的unicode字符串
 
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
+
     def test_home_page_can_save_a_POST_request(self):
         request = HttpRequest()
         request.method = 'POST'
         request.POST['item_text'] = 'A new list item'
 
         response = home_page(request)
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
         self.assertIn('A new list item', response.content.decode())
         expected_html = render_to_string(
             'home.html',
