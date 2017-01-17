1a0410b856d27aeb909bfcd4b0e389355604c678
viprs <viprs@qq.com>
2017-01-17 17:17:32 +0800

    添加输入框居中显示的测试

--- superlists/function_tests/tests.py
+++ superlists/function_tests/tests.py
@@ -90,4 +90,25 @@ class NewVistorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # 两个人都很满意，睡觉去了
-        self.fail('Finish the test!')
+        #self.fail('Finish the test!')
+    def test_layout_and_styling(self):
+        # Alice访问首页
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+        
+        #输入框完美地居中显示
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width']/2,
+            512,
+            delta=5
+        )
+        
+        #她新建了一个清单，看到输入框完美地居中显示
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width']/2,
+            512,
+            delta=5
+        )
+
